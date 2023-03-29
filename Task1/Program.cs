2ff8c7df37d1bb767b20404fe859f5a05b9d37e0
Evgeniya.S <reg.subbotina@gmail.com>
2023-03-29 21:39:39 +0300

    added Task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,20 @@
+﻿/*Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1*/
+
+int a = GetRandom();
+System.Console.WriteLine(a);
+System.Console.WriteLine(GetSecondDigit(a));
+
+int GetRandom()
+{
+    return new Random().Next(100, 1000);
+}
+
+int GetSecondDigit(int number)
+{
+    int FirstTwoNumbers = number / 10;
+    int SecondDigit = FirstTwoNumbers % 10;
+    return SecondDigit;
+}
